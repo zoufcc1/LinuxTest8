babf945884665a8d17ee2cd9c355f143012224df
zoufcc1 <1756842005@qq.com>
2021-11-07 22:38:02 +0800

    linux test

--- testx.sh
+++ testx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-read -a arr
-for ((i=1;i < ${#arr[*]};i++))
+read -a score
+for ((i=1;i < ${#score[*]};i++))
 do
- for((j=0;j < ${#arr[*]}-$i;j++))
- do
-  if [ ${arr[j]} -gt ${arr[(($j+1))]} ]
-  then temp=${arr[j]}
-   arr[j]=${arr[$(($j+1))]}
-   arr[$(($j+1))]=$temp
-  fi
- done
+	for((j=0;j < ${#score[*]}-$i;j++))
+	do
+		if [ ${score[j]} -gt ${score[(($j+1))]} ]
+		then	temp=${score[j]}
+			score[j]=${score[$(($j+1))]}
+			score[$(($j+1))]=$temp
+		fi
+	done
 done
-echo ${arr[*]}
+echo ${score[*]}
